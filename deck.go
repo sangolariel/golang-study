54925b27ca07502e4a03d1c196286533725110b4
sangnh <sangnh@futurify.vn>
2021-03-28 13:27:51 +0700

    Random number gere

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of Deck
@@ -54,8 +55,10 @@ func newDeckFromFile(fileName string) deck {
 }
 
 func (d deck) shuffe() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
