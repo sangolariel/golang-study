d361f127afbbcaea43d2b2005f33bbcc7ef3d7fc
sangnh <sangnh@futurify.vn>
2021-03-31 23:09:37 +0700

    Declaring Struct

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstname string
 	lastname  string
 }
 
 func main() {
-
+	alex := person{firstname: "Alex", lastname: "Anderson"}
+	fmt.Println(alex)
 }
