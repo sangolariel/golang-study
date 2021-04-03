2fcbdb569131ab590958a98bc4b4fe991cbe5bca
sangnh <sangnh@futurify.vn>
2021-04-03 22:39:08 +0700

    Lesson 46. Manipulating Maps

--- map/main.go
+++ map/main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "$4bf645",
-	}
+
+	colors := make(map[string]string) // make an empty map
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white") // delete a key value pair
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "$4bf645",
+	// }
 	fmt.Println(colors)
 }
