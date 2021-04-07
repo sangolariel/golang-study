33b88ce42bae7bead06938d3a92fa642ba166bc5
sangnh <sangnh@futurify.vn>
2021-04-08 00:06:39 +0700

    Lesson 66. Printing Site Status

--- chanels/main.go
+++ chanels/main.go
@@ -1,12 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	links := []string {
+	links := []string{
 		"http://google.com",
 		"http://golang.org",
 	}
 
-	for _, link := range {
-
+	for _, link := range links {
+		checkLinks(link)
 	}
+}
+
+func checkLinks(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+	fmt.Println(link, "is up!")
 }
